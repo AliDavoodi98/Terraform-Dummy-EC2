525f62ca9a4a3ce98ff86eb56770f7713c56b7ca
alidavoodimoghadam <alidavid797@gmail.com>
2024-11-12 20:52:37 -0800

    create the internet gateways and make the subnet public

--- main.tf
+++ main.tf
@@ -21,6 +21,24 @@ resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
 
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "public_rt_assoc" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
 resource "aws_subnet" "public_subnet" {
   vpc_id = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
@@ -55,4 +73,8 @@ resource "aws_vpc_security_group_ingress_rule" "ssh" {
   from_port = 22
   ip_protocol = "tcp"
   to_port = 22
+}
+
+output "ec2_private_IP" {
+  value = aws_instance.ec2.private_ip
 }
