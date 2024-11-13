90de92efceeac0292ab4b8209453d0b0310e0a19
alidavoodimoghadam <alidavid797@gmail.com>
2024-11-12 20:19:35 -0800

    fix the ip protocol

--- main.tf
+++ main.tf
@@ -44,6 +44,6 @@ resource "aws_vpc_security_group_ingress_rule" "ssh" {
   security_group_id = aws_security_group.allow_ssh.id
   cidr_ipv4 = "${data.http.my_ip.body}/32"
   from_port = 22
-  ip_protocol = "ssh"
+  ip_protocol = "tcp"
   to_port = 22
 }
