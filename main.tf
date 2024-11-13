bdc012d632399ce5ebe7b6d73b79c8198a91b3d8
alidavoodimoghadam <alidavid797@gmail.com>
2024-11-12 20:31:14 -0800

    setting up the subnet to associate it with the ec2 and sg

--- main.tf
+++ main.tf
@@ -21,9 +21,17 @@ resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
 
+resource "aws_subnet" "public_subnet" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+}
+
 resource "aws_instance" "ec2" {
   ami = data.aws_ami.ubuntu_ami.id
   instance_type = "t3.micro"
+  subnet_id = aws_subnet.public_subnet.id
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
 
   tags = {
     "Name" = "Main EC2"
