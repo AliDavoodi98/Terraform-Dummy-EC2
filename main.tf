4c7379732dc431766c633b60afb78e112ed9ccee
alidavoodimoghadam <alidavid797@gmail.com>
2024-11-12 20:12:08 -0800

    added security_group

--- main.tf
+++ main.tf
@@ -13,6 +13,9 @@ data "aws_ami" "ubuntu_ami" {
   owners = [ "amazon" ]
 }
 
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
 
 resource "aws_instance" "ec2" {
   ami = data.aws_ami.ubuntu_ami.id
@@ -21,4 +24,22 @@ resource "aws_instance" "ec2" {
   tags = {
     "Name" = "Main EC2"
   }
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name = "sg_allow_ssh"
+  description = "This Security Group should allow SSH connection from the main host to the EC2 Instance"
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "sg_allow_ssh"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "ssh" {
+  security_group_id = aws_security_group.allow_ssh.id
+  cidr_ipv4 = aws_vpc.main.id
+  from_port = 22
+  ip_protocol = "ssh"
+  to_port = 22
 }
