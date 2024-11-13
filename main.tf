b57248cb2c8da82d358350ddd49d47b3b0ed4a9b
alidavoodimoghadam <alidavid797@gmail.com>
2024-11-12 19:55:02 -0800

    adding data source

--- main.tf
+++ main.tf
@@ -2,7 +2,23 @@ provider "aws" {
   region = "us-east-1"
 }
 
+data "aws_ami" "ubuntu_ami" {
+  most_recent = true 
+
+  filter {
+    name = "name"
+    values = [ "ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*" ]
+  }
+
+  owners = [ "amazon" ]
+}
+
+
 resource "aws_instance" "ec2" {
-  ami = ""
-  
+  ami = data.aws_ami.ubuntu_ami.id
+  instance_type = "t3.micro"
+
+  tags = {
+    "Name" = "Main EC2"
+  }
 }
