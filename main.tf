0779d1c18b1402a1c23b5027b769b437fbe60456
alidavoodimoghadam <alidavid797@gmail.com>
2024-11-12 20:16:45 -0800

    allow ssh connection from local host

--- main.tf
+++ main.tf
@@ -42,7 +42,7 @@ resource "aws_security_group" "allow_ssh" {
 
 resource "aws_vpc_security_group_ingress_rule" "ssh" {
   security_group_id = aws_security_group.allow_ssh.id
-  cidr_ipv4 = aws_vpc.main.id
+  cidr_ipv4 = "${data.http.my_ip.body}/32"
   from_port = 22
   ip_protocol = "ssh"
   to_port = 22
